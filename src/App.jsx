0f01f02ebe985f649994490f60f086767e4be2f5
zednegr <negrzed@gmail.com>
2023-07-26 04:25:53 +0500

    first update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/header/header'
 
 function App() {
 
   return (
     <>
-
+      <Header />
     </>
   )
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import './header.css'
+
+function Header() {
+    
+    return (
+        <>
+            <div className="main">
+                <button className="btn">Hover Me</button>
+            </div>
+        </>
+    )
+}
+
+export default Header
